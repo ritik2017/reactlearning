2a2cd48a657b20ceb6d1c63f33d015b25e33a01b
Ritik Kumar <kumarritik@microsoft.com>
2023-01-16 20:36:08 +0530

    Todo app

--- src/TodoFuncComp/Task.js
+++ src/TodoFuncComp/Task.js
@@ -1,10 +1,24 @@
 // import './Task.css';
 
 export default function Task(props) {
+
+    function handleMarkCompleted() {
+        props.updateTask(props.index);
+    }
+
+    function handleRemoveTask() {
+        props.removeTask(props.index);
+    }
+
     return (
-        <div className="task" style={{width: '100px', background: 'blue', maxWidth: '200px'}}>
-            {props.title}
-            {props.completed && <button> Completed</button>}
+        <div className="task" style={{background: '#D3D3D3', maxWidth: '400px', display: 'flex', justifyContent: 'space-between'}}>
+            <div style={{height: '30px', fontSize: '22px'}}>{props.title} </div>
+            <div style={{display: 'flex', justifyContent: 'end'}}>
+                {props.completed && <button style={{border: 'transparent', background: 'green'}}>Completed</button>}
+                {!props.completed && <button onClick={handleMarkCompleted}>Mark Completed</button>}
+                <button style={{background: 'red'}} onClick={handleRemoveTask}>Remove Task</button>
+            </div>
+            
         </div>
     )
 }
--- src/TodoFuncComp/Todo.js
+++ src/TodoFuncComp/Todo.js
@@ -24,14 +24,33 @@ export default function Todo() {
         })
     }
 
+    function removeTask(index) {
+        setTasks(tasks => {
+            const newTasks = [...tasks];
+            newTasks.splice(index, 1);
+            return newTasks;
+        })
+    }
+
+    function updateTask(index) {
+        setTasks(tasks => {
+            // tasks[index].completed = true;
+            // return tasks;
+            const newTasks = [...tasks];
+            newTasks[index].completed = true;
+
+            return newTasks;
+        })
+    }
+
     return (
         <div className="todo-container">
-            <div className="pending-todo">
-                <h2>Pending Todos</h2>
-                {tasks.map((task, index) => <Task {...task} key={index}/> )}
-            </div>
             <div className="create-todo">
                 <CreateTask addTask={addTask}/>
+            </div>
+            <div className="pending-todo">
+                <h2>Pending Todos</h2>
+                {tasks.map((task, index) => <Task {...task} key={index} index={index} updateTask={updateTask} removeTask={removeTask}/> )}
             </div>
         </div>
     )
